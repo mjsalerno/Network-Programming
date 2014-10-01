15efd34883273f0415ceb525ea57353d150442fc
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-09-30 20:12:20 -0400

    added lots of error checks and made the time server (no I didnt)

--- lab1/server.c
+++ lab1/server.c
@@ -2,11 +2,17 @@
 
 int main(void) {
 
+    fd_set fdset;
+    struct timeval time;
+    int newfd;
+    socklen_t addrlen;
+
     struct sockaddr_in timesrv;
     struct sockaddr_in echosrv;
     struct thread_args targs;
     int echo_listen_fd;
     int time_listen_fd;
+    int err;
     pthread_t thread;
 
     //zero out struct
@@ -26,80 +32,110 @@ int main(void) {
     echo_listen_fd = socket(AF_INET,SOCK_STREAM,0);
     time_listen_fd = socket(AF_INET,SOCK_STREAM,0);
 
+    if(echo_listen_fd < 3 || time_listen_fd < 3) {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
     //bind
-    bind(echo_listen_fd,(struct sockaddr *)&echosrv,sizeof(echosrv));
-    bind(time_listen_fd,(struct sockaddr *)&timesrv,sizeof(timesrv));
+    err = bind(echo_listen_fd,(struct sockaddr *)&echosrv,sizeof(echosrv));
+    if(err != 0) {
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
+
+    err = bind(time_listen_fd,(struct sockaddr *)&timesrv,sizeof(timesrv));
+    if(err != 0) {
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
 
     //listen
-    listen(echo_listen_fd, BACKLOG);
-    listen(time_listen_fd, BACKLOG);
+    err = listen(echo_listen_fd, BACKLOG);
+    if(err != 0) {
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
 
-    int fdrdy = 0;
-    fd_set fdset;
-    struct timeval time;
-    int newfd;
-    socklen_t addrlen;
+    err = listen(time_listen_fd, BACKLOG);
+    if(err != 0) {
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
 
-    time.tv_sec = 10;
-    time.tv_usec = 0;
+    for(;;) {
+        time.tv_sec = 10;
+        time.tv_usec = 0;
 
-    FD_ZERO(&fdset);
-    FD_SET(STDIN_FILENO  , &fdset);
-    FD_SET(echo_listen_fd, &fdset);
-    FD_SET(time_listen_fd, &fdset);
+        FD_ZERO(&fdset);
+        FD_SET(STDIN_FILENO, &fdset);
+        FD_SET(echo_listen_fd, &fdset);
+        FD_SET(time_listen_fd, &fdset);
 
-    fdrdy = select(2, &fdset, NULL, NULL, &time);
-    printf("this many fds are ready: %d\n", fdrdy);
+        err = select(time_listen_fd + 1, &fdset, NULL, NULL, &time);
+        if(err < 0) {
+            perror("select()");
 
-    if(FD_ISSET(echo_listen_fd, &fdset)) {
-        printf("setting up a echo server\n");
-        addrlen = sizeof(echosrv);
-        newfd = accept(echo_listen_fd, (struct sockaddr *)&echosrv, &addrlen);
-        targs.connfd = newfd;
-
-        int iret1 = pthread_create(&thread, NULL, (void*)&time_server, &targs);
-        if (iret1) {
-            fprintf(stderr, "Error - pthread_create() return code: %d\n", iret1);
-            exit(EXIT_FAILURE);
         }
-    }
 
-    if(FD_ISSET(time_listen_fd, &fdset)) {
-        printf("setting up a time server\n");
-    }
+        if (FD_ISSET(echo_listen_fd, &fdset)) {
+            printf("setting up a echo server\n");
+            addrlen = sizeof(echosrv);
+            newfd = accept(echo_listen_fd, (struct sockaddr *) &echosrv, &addrlen);
+            targs.connfd = newfd;
 
-    if(FD_ISSET(STDIN_FILENO, &fdset)) {
-        printf("Don't type things here\n");
+            int iret1 = pthread_create(&thread, NULL, (void *) &echo_server, &targs);
+            if (iret1) {
+                fprintf(stderr, "Error - pthread_create() return code: %d\n", iret1);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if (FD_ISSET(time_listen_fd, &fdset)) {
+            printf("setting up a time server\n");
+            addrlen = sizeof(timesrv);
+            newfd = accept(time_listen_fd, (struct sockaddr *) &echosrv, &addrlen);
+            targs.connfd = newfd;
+
+            int iret1 = pthread_create(&thread, NULL, (void *) &time_server, &targs);
+            if (iret1) {
+                fprintf(stderr, "Error - pthread_create() return code: %d\n", iret1);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &fdset)) {
+            while ( getchar() != '\n' );
+            printf("Don't type things here\n");
+        }
     }
 
     return EXIT_SUCCESS;
 }
 
-void time_server(struct thread_args *targs) {
+void echo_server(struct thread_args *targs) {
     int n = 1;
     char buffer[BUFF_SIZE];
 
     while(n > 0) {
         n = recv(targs->connfd, buffer, BUFF_SIZE, 0);
         buffer[n] = 0;
-        send(targs->connfd, buffer, strlen(buffer), 0);
+        send(targs->connfd, "hello\n", 7, 0);
     }
-
+    printf("closing the echo server\n");
     close(targs->connfd);
 
 }
 
-int max(int a, int b) {
-    return a > b ? a : b;
-}
+void time_server(struct thread_args *targs) {
 
-void my_fd_set(fd_set *fdset, int *arr, int size) {
-    int i;
+    char *word = "hello\n";
 
-    for(i = 0; i < size; i++) {
-        if(*(arr + i)) {
-            printf("setting: %d\n", *(arr + i));
-            FD_SET(i, fdset);
-        }
+    while(1) {
+        send(targs->connfd, word, strlen(word), 0);
+        sleep(5);
     }
+    printf("closing the time server\n");
+    close(targs->connfd);
+
 }
