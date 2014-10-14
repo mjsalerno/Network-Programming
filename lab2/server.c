449ab4828fa3816279b1f3022e963c69cf5797cc
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-10-14 01:14:26 -0400

    added code to send stuff

--- lab2/server.c
+++ lab2/server.c
@@ -42,5 +42,36 @@ int main(int argc, const char **argv) {
     printf("config: %s\n", path);
 
     fclose(file);
+
+
+
+
+    int sockfd,n;
+    struct sockaddr_in servaddr,cliaddr;
+    socklen_t len;
+    char mesg[BUFF_SIZE];
+
+    sockfd=socket(AF_INET,SOCK_DGRAM,0);
+
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons();
+    bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+
+    for (;;) {
+        len = sizeof(cliaddr);
+        n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
+        sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
+        printf("-------------------------------------------------------\n");
+        mesg[n] = 0;
+        printf("Received the following:\n");
+        printf("%s",mesg);
+        printf("-------------------------------------------------------\n");
+    }
+
+
+
+
     return EXIT_SUCCESS;
 }
