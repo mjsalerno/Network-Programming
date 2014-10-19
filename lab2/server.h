446b2b0ebc557fc093e0e464f0539e4db268f0be
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-10-19 14:16:16 -0400

    adds the client_list struct

--- lab2/server.h
+++ lab2/server.h
@@ -4,7 +4,14 @@
 #include "common.h" 
 
 #define DEBUG
-#include "debug.h" 
+#include "debug.h"
+
+struct client_list {
+    char* ip;
+    uint16_t port;
+    pid_t pid;
+    struct client_list* next;
+};
 
 int child(int parent_sock);
 
