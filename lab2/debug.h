7bde4b98938c5aaa5c4d3c28974eb5413283007e
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-10-19 17:43:48 -0400

    prints DEBUG before all of the debug prints so I stop thinking they are compile errors

--- lab2/debug.h
+++ lab2/debug.h
@@ -1,7 +1,7 @@
 #ifndef DEBUG_H
     #define DEBUG_H
 #ifdef DEBUG
-    #define _DEBUG(fmt, args...) printf("%s:%s:%d: "fmt, __FILE__, __FUNCTION__, __LINE__, args)                                                                          
+    #define _DEBUG(fmt, args...) printf("DEBUG: %s:%s:%d: "fmt, __FILE__, __FUNCTION__, __LINE__, args)
 #else
     #define _DEBUG(fmt, args...)
 #endif
