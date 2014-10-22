7b5891845608c50016c4720f049770fffc479999
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-10-22 18:58:47 -0400

    Adds xtcp to the make files

--- lab2/xtcp.h
+++ lab2/xtcp.h
@@ -1,3 +1,9 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#ifndef XTCP_H
+#define XTCP_H
+
 #define FIN 0x1
 #define SYN 0x2
 #define RST 0x4
@@ -9,9 +15,9 @@ struct xtcphdr {
     uint32_t ack_seq;
     uint16_t flags;
     uint16_t win_size;
-
+    char data[500];
 };
 
 void print_xtxphdr(struct xtcphdr *hdr);
 
-
+#endif /*XTCP_H*/
