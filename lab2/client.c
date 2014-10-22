4a150f4f0702ad1764d9593e2fdb84005f387a35
Scott Harvey <scott.harvey@stonybrook.edu>
2014-10-21 23:21:27 -0400

    Error: can't reconnect?!?!?!

--- lab2/client.c
+++ lab2/client.c
@@ -2,8 +2,8 @@
 
 int main(void) {
     /*stuff mike added*/
-    socklen_t len;
-    ssize_t n;
+    /*socklen_t len;
+    ssize_t n;*/
     /*stuff mike added*/
 
     ssize_t err; /* for error checking */
@@ -11,7 +11,7 @@ int main(void) {
     char transferpath[BUFF_SIZE]; /* file to transfer */
     FILE *file; /* config file */
     char ip4_str[INET_ADDRSTRLEN];
-    char buf[BUFF_SIZE + 1];
+    /* char buf[BUFF_SIZE + 1]; */
     /* config/xtcp vars */
     uint16_t windsize;
     int seed;
@@ -20,9 +20,6 @@ int main(void) {
 
     /* serv_fd -- the main server connection socket, reconnected later */
     int serv_fd;
-    /* select vars */
-    fd_set rset;
-    int maxfpd1 = 0;
 
     uint16_t knownport; /* , trans_port; */
     /* my_addr -- my (client) address */
@@ -55,7 +52,7 @@ int main(void) {
         "client.in:1: error getting IPv4 address");
     err = inet_pton(AF_INET, ip4_str, &(serv_addr.sin_addr));
     if(err <= 0){
-        fprintf(stderr, "client.inet_pton() invalid IPv4 address\n");
+        fprintf(stderr, "inet_pton() invalid IPv4 address\n");
         fclose(file);
         exit(EXIT_FAILURE);
     }
@@ -87,68 +84,114 @@ int main(void) {
     /* get a socket to talk to the server */
     serv_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if(serv_fd < 0){
-        perror("client.bind()");
+        perror("bind()");
         exit(EXIT_FAILURE);
     }
     /* bind to my ip */
     err = bind(serv_fd, (struct sockaddr *)&my_addr, sizeof(my_addr));
     if(err < 0){
-        perror("client.bind()");
+        perror("bind()");
         exit(EXIT_FAILURE);
     }
 
     /* call on bind'ed socket getsockname(), i.e. print_sock_name */
-    printf("client bind()'ed to -- ");
+    printf("bind()'ed to -- ");
     print_sock_name(serv_fd, &bind_addr);
 
     /* connect to server ip */
     err = connect(serv_fd, (const struct sockaddr*)&serv_addr, sizeof(serv_addr));
     if(err < 0){
-        perror("client.connect()");
+        perror("connect()");
         exit(EXIT_FAILURE);
     }
 
     /* call on connect'ed socket getpeername(), i.e. print_sock_peer */
-    printf("client connect()'ed to -- ");
+    printf("connect()'ed to -- ");
     print_sock_peer(serv_fd, &peer_addr);
 
-    strncpy(buf, "SEND ACK 1 SEQ 0", sizeof(buf));
-
-
-    /* print_xtxphdr(&hdr); */
-    /* todo: timeout  on oldest packet */
-
-    /* simulate packet loss on sends */
-    if(drand48() > p) {
-        err = send(serv_fd, buf, strlen(buf), 0);
-        if (err < 0) {
-            perror("client.sendto()");
-            close(serv_fd);
-            exit(EXIT_FAILURE);
-        }
-        printf("Sent connection req to server\n");
-    }
-
-    /* select() for(ever) for SYN */
-    for(;;) {
-        FD_ZERO(&rset);
-        FD_SET(serv_fd, &rset);
-        maxfpd1 = serv_fd + 1;
-
-        /* todo: liveliness timer? */
-        select(maxfpd1, &rset, NULL, NULL, NULL);
-
-        /*stuff mike added*/
-        _DEBUG("%s\n", "got SYN-ACK or something, next recvfrom ...");
-        len = sizeof(serv_addr);
-        n = recvfrom(serv_fd, buf, sizeof(buf), 0, (struct sockaddr *)&serv_addr, &len);
-        buf[n] = 0;
-        printf("new port: %hu\n", *((unsigned short *)buf));
-        break;
-        /*stuff mike added*/
+    err = handshake_first_sec(p, serv_fd, &serv_addr);
+    if(err < 0){
+        /* todo: clean up, close?, free?*/
+        close(serv_fd);
+        exit(EXIT_FAILURE);
     }
 
     close(serv_fd);
 
     return EXIT_SUCCESS;
 }
+
+int handshake_first_sec(double p, int serv_fd, struct sockaddr_in *serv_addr){
+    char buf[BUFF_SIZE + 1];
+    char ip4_str[INET_ADDRSTRLEN];
+    /* select vars */
+    fd_set rset;
+    int maxfpd1 = 0;
+
+    ssize_t n, err;
+
+    strncpy(buf, "SEND ACK 1 SEQ 0", sizeof(buf));
+
+
+    /* todo: print_xtxphdr(&hdr); */
+    /* todo: timeout  on oldest packet */
+
+    /* simulate packet loss on sends */
+    if(drand48() > p) {
+        err = send(serv_fd, buf, strlen(buf), 0);
+        if (err < 0) {
+            perror("sendto()");
+            close(serv_fd);
+            exit(EXIT_FAILURE);
+        }
+        printf("sent connection req to server\n");
+    }
+
+    /* select() for(ever) for SYN */
+    for(;;) {
+        in_port_t newport;
+        FD_ZERO(&rset);
+        FD_SET(serv_fd, &rset);
+        maxfpd1 = serv_fd + 1;
+
+        /* todo: liveliness timer? RTO/RTT timer */
+        select(maxfpd1, &rset, NULL, NULL, NULL);
+        /* check if the serv_fd is set */
+        if(FD_ISSET(serv_fd, &rset)) {
+            _DEBUG("%s\n", "got SYN-ACK or something, next recvfrom ...");
+            printf("waiting for second handshake...\n");
+            n = recv(serv_fd, buf, sizeof(buf), 0);
+            if (n < 0) {
+                perror("recv()");
+                return -1;
+            }
+            /* copy the passed port into a short, prevents SIGBUS */
+            memcpy(&newport, buf, sizeof(newport));
+            serv_addr->sin_port = newport; /* set the new port */
+            serv_addr->sin_family = AF_INET;
+            /* re connect() with new port */
+            printf("serv_addr->sin_family: %d (AF_INET=%d)\n", serv_addr->sin_family, AF_INET);
+            /* fixme: disconnecting socket first doesn't help */
+
+            if(NULL == inet_ntop(AF_INET, (void *)&(serv_addr->sin_addr), ip4_str, sizeof(ip4_str))){
+                perror("inet_ntop()");
+                return -1;
+            }
+            printf("connect addr: %s:%hu\n", ip4_str, ntohs(serv_addr->sin_port));
+
+            err = connect(serv_fd, (const struct sockaddr*)&serv_addr, sizeof(struct sockaddr));
+            if (err < 0) {
+                perror("re connect()");
+                return -1;
+            }
+            printf("re connect()'d to new server port: %hu\n", newport);
+            return 0;
+        }
+    }
+}
+
+/*
+int handshake_second(int serv_fd, struct sockaddr_in* serv_addr){
+    return 0;
+}*/
+
--- lab2/client.h
+++ lab2/client.h
@@ -7,4 +7,11 @@
 #define DEBUG
 #include "debug.h"
 
+/*
+Send the first handshake and recv the second.
+Return: 0 on success
+        -1 on error, with perror() printed
+*/
+int handshake_first_sec(double p, int serv_fd, struct sockaddr_in *serv_addr);
+
 #endif
