b2bd1acee6a9120707ab3815336584362517caa0
Scott Harvey <scott.harvey@stonybrook.edu>
2014-10-29 21:56:19 -0400

    Add color print out for _ERROR macro.

--- lab2/debug.h
+++ lab2/debug.h
@@ -1,10 +1,18 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#define _RED     "\x1b[31m"
+#define _GREEN   "\x1b[32m"
+#define _YELLOW  "\x1b[33m"
+#define _BLUE    "\x1b[34m"
+#define _RESET   "\x1b[0m"
+
 #ifdef DEBUG
 # define _DEBUG(fmt, args...) printf("DEBUG: %s:%s:%d: "fmt, __FILE__, __FUNCTION__, __LINE__, args)
 #else
 # define _DEBUG(fmt, args...)
 #endif
+
+#define _ERROR(fmt, args...) fprintf(stderr, _RED "ERROR: %s:%s:%d: "fmt _RESET, __FILE__, __FUNCTION__, __LINE__, args)
 
 #endif
