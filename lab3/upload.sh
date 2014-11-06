bba69fa5d017c96d4d7e35736c4de8aef53b8b5c
Scott Harvey <scott.harvey@stonybrook.edu>
2014-11-06 18:45:04 -0500

    ./upload.sh cse533-XX  # to scp all files to minix

---
+++ lab3/upload.sh
@@ -0,0 +1,18 @@
+# scp all files to minix
+
+# must give login name for minix
+if test "$#" -ne 1; then
+	echo "usage: $0 minixlogin"
+	return
+fi
+
+# transfer the makefile and all .c/.h
+FILES=$(ls *.c *.h makefile)
+
+# remove the newlines
+PRJFILES=$(echo $FILES | tr '\n' ' ')
+
+echo $PRJFILES
+# must have minix in ~/.ssh/config
+# 130.245.156.19
+scp $PRJFILES $1@minix:/users/cse533/students/$1/cse533
