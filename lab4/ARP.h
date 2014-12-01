43608cefd853f706d1a70a1a42e9937814760718
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-12-01 10:51:42 -0500

    made arp_cache ??

--- lab4/ARP.h
+++ lab4/ARP.h
@@ -1,4 +1,15 @@
 #ifndef ARP_H
 #define ARP_H
 
+#include "common.h"
+#include "get_hw_addrs.h"
+
+struct arp_cache {
+    unsigned char      sll_addr[8];  /* Physical layer address */
+    struct   sockaddr  *ip_addr;	 /* IP address */
+    struct arp_cache* next;
+};
+
+/*todo: make the actually arp thing*/
+
 #endif
