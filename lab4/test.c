17ef3dc51b65d869c0b154b46a5760f9b0f0287a
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-12-03 18:34:58 -0500

    added a test case for add_arp

--- lab4/test.c
+++ lab4/test.c
@@ -1,15 +1,18 @@
 #include <assert.h>
 #include "ping.h"
+#include "ARP.h"
 #include "common.h"
 
-void testping ();
+void test_csum ();
+void test_add_arp();
 
 int main() {
-    testping();
+    test_csum();
+    test_add_arp();
     return 0;
 }
 
-void testping () {
+void test_csum() {
 
     //0x4500 0x003c 0x1c46 0x4000 0x4006 0xb1e6 0xac10 0x0a63 0xac10 0x0a0c -> 0xB1E6
 
@@ -29,4 +32,23 @@ void testping () {
     uint16_t mycsum = csum(fake_ip, 20);
 
     assert(mycsum == 0xB1E6);
+}
+
+void test_add_arp() {
+    int err;
+    struct arp_cache* arp_head = NULL;
+    struct in_addr sin_addr;
+
+    unsigned char mac1[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00};
+
+    err = inet_aton("123.234.213.9", &sin_addr);
+    assert(err != 0);
+
+    assert(arp_head == NULL);
+    add_arp(&arp_head, sin_addr.s_addr, 1, 0xF, 6, mac1);
+    assert(arp_head != NULL);
+    assert(arp_head->next == NULL);
+
+    add_arp(&arp_head, sin_addr.s_addr, 1, 0xF, 6, mac1);
+    assert(arp_head->next->next == NULL);
 }
